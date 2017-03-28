8f5ce58d2d7430e0bd8ba69d6b0164e97e7ce54f
José Javier <josejavierdaw2015@gmail.com>
2017-03-28 12:44:37 +0200

    añado enlaces a la lista de grupos y añado efecto de transición

--- js/animacionGrupos.js
+++ js/animacionGrupos.js
@@ -1,9 +1,13 @@
 $("nav#botonesGrupo ul li").click(function(){
 
-    $("#grupos").animate({height:0},"fast");
-    $("#grupos").animate({width:0},"fast");
-    $("#grupos").animate({width:770},"fast");
-    $("#grupos").animate({height:400},"fast");
+    $("#grupos").animate({height:0}, 500, "easeInBack");
+    $("#grupos").animate({width:0},500, "easeInBack");
+    $("#grupos").animate({width:770},600, "easeOutBack");
+    $("#grupos").animate({height:400},800, "easeOutBounce");
     
+    var enlace = $(this).attr("enlace");
+
+    $("#grupos img").hide(300);
+    $(enlace).delay(500).show(100);
 
 });
