24a16459e2517559e2c4ee698dee680fe7186d27
José Javier <josejavierdaw2015@gmail.com>
2017-03-28 12:22:52 +0200

    animacion del cartel de los grupos

---
+++ js/animacionGrupos.js
@@ -0,0 +1,9 @@
+$("nav#botonesGrupo ul li").click(function(){
+
+    $("#grupos").animate({height:0},"fast");
+    $("#grupos").animate({width:0},"fast");
+    $("#grupos").animate({width:770},"fast");
+    $("#grupos").animate({height:400},"fast");
+    
+
+});
